29a68b06c53534bcfcecc10a0bb13ecfd41d5c30
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2021-11-27 20:38:32 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Rivas-Component-Library
+Personal library of components. All made with html5, scss, and vanilla javascript.
---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+// Navigation 1
+const body = document.querySelector('body');
+const burger = document.querySelector(".hamburger");
+const navOverlay = document.querySelector('.navlinks-overlay');
+const nav = document.querySelector(".nav-links");
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('is-active');
+    burger.classList.toggle('is-active');
+    body.classList.toggle('body-fixed');
+    navOverlay.classList.toggle('is-active');
+})
+navOverlay.addEventListener('click', () => {
+    nav.classList.toggle('is-active');
+    burger.classList.toggle('is-active');
+    body.classList.toggle('body-fixed');
+    navOverlay.classList.toggle('is-active');
+})
+// // Color theme toggle
+// const themeToggle = document.querySelector('.theme-toggle');
+// const prefersDarkTheme = window.matchMedia("(prefers-color-scheme: dark)");
+// const currentTheme = localStorage.getItem('theme');
+
+// if (currentTheme == 'dark') {
+//     document.body.classList.toggle('dark-mode')
+// }
+
+// themeToggle.addEventListener('click', () => {
+//     if (prefersDarkTheme.matches) {
+//         document.body.classList.toggle('light-mode')
+//         var theme = document.body.classList.contains('light-mode') ? 'light' : 'dark'
+//     } else {
+//         document.body.classList.toggle('dark-mode');
+//         var theme = document.body.classList.contains('dark-mode') ? 'dark' : 'light'
+//     }
+//     localStorage.setItem('theme', theme)
+// })
